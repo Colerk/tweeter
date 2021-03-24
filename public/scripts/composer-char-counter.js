91e9058aefac08d7f0b3fe3391175a2f5a8fe1ea
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-24 00:36:41 +0000

    initial layouts done for home page

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  len = 140
+  $('#counter').html(len)
+  $('textarea').keyup(function() {
+    if ($(this).val().length > len) {
+      $('#counter').html(len-$(this).val().length).css("color", "red");
+    } else {
+      $('#counter').html(len-$(this).val().length).css("color", "#545149");
+    }
+  })
+});
---
+++ public/scripts/hover.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   $("article").hover(function() {
+//     if ($(this).length) {
+//       $("usernameHide").toggeClass("usernameShow")
+//     // } else {
+//     //   $("header").removeClass("usernameShow")
+//     // }
+//   })
+// })
